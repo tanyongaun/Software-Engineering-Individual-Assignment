e8f33dcb5ee5564bfff9e509ce2315d4fc1c16a2
Yong Aun Tan <tanyongaun0123@e.newera.edu.my>
2023-01-10 20:00:02 +0800

    first commit

---
+++ complaint management system/cms/admin/include/footer.php
@@ -0,0 +1,7 @@
+	<div class="footer">
+		<div class="container">
+			 
+
+			<b class="copyright">&copy; 2022 New Era University College </b> All rights reserved.
+		</div>
+	</div>
---
+++ complaint management system/cms/users/includes/footer.php
@@ -0,0 +1,8 @@
+ <footer class="site-footer">
+          <div class="text-center">
+          2022 - New Era University College
+              <a href="#" class="go-top">
+                  <i class="fa fa-angle-up"></i>
+              </a>
+          </div>
+      </footer>
